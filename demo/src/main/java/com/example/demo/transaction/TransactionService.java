4c866b6e4fa8d0c042217fb3b7c25e69f7d992ef
sang-uk-jang <tkddnr2266@konkuk.ac.kr>
2026-03-10 11:16:30 +0900

    feat: adding get Transaction by Amount
    Amount를 통해 수익과 지출을 거래 내역을 불러오는 기능을 추가하였습니다.

--- demo/src/main/java/com/example/demo/transaction/TransactionService.java
+++ demo/src/main/java/com/example/demo/transaction/TransactionService.java
@@ -68,4 +68,38 @@ public class TransactionService {
                 .toList();
     }
 
+    public List<TransactionDto.transactionResponse> getAmountTrans(CategoryType categoryType, String email) {
+        User user =userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+
+        if(categoryType == CategoryType.INCOME){
+            List<Transaction> IncomeTransactions = transactionRepository
+                    .getIncomeAmountTrans(user.getId());
+            return IncomeTransactions.stream()
+                    .map(t -> new TransactionDto.transactionResponse(
+                            t.getId(),
+                            t.getAmount(),
+                            t.getTransaction_date(),
+                            t.getCategory_id(),
+                            t.getSource_id(),
+                            t.getMemo()
+                    ))
+                    .toList();
+        }
+        else{
+            List<Transaction> ExpenseTransactions = transactionRepository
+                    .getExpenseAmountTrans(user.getId());
+            return ExpenseTransactions.stream()
+                    .map(t -> new TransactionDto.transactionResponse(
+                            t.getId(),
+                            t.getAmount(),
+                            t.getTransaction_date(),
+                            t.getCategory_id(),
+                            t.getSource_id(),
+                            t.getMemo()
+                    ))
+                    .toList();
+        }
+    }
+
 }
